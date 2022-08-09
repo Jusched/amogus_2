b56e739e06c3fb882d4707cffde0f06103098946
Juan Sebastian Betancur <52014810+Jusched@users.noreply.github.com>
2022-08-08 19:25:29 -0500

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import random
-
-current_players = ["Blue", "Red", "Orange", "Green"]
-
-
-unlucky = current_players.pop(random.randint(0, len(current_players)-1))
-print(f"{unlucky} has been ejected. ")
